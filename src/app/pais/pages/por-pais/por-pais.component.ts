634f2cb511f9ce0b7f3c4387275909b3e8a511dd
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-03-16 19:57:46 +0100

    add pais service and paises search

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,10 +10,25 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
 
-  termino: string = 'Hola';
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService ) { }
 
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      }, (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    )
   }
 
 }

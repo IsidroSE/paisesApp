298e021a75b318ca6b356f2364e4aaae3b7e7062
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-03-19 11:11:00 +0100

    add porPais page

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
   styles: [
+    `
+    .mr-1 {
+      margin-right: 5px;
+    }
+    `
   ]
 })
 export class VerPaisComponent implements OnInit {
+
+  pais!: Country;
 
   constructor( 
     private activatedRoute: ActivatedRoute,
@@ -20,11 +28,10 @@ export class VerPaisComponent implements OnInit {
 
     this.activatedRoute.params
     .pipe(
-      switchMap( ({id}) => this.paisService.getPaisPorAlpha( id ) )
+      switchMap( ({id}) => this.paisService.getPaisPorAlpha( id ) ),
+      tap( console.log )
     )
-    .subscribe( resp => {
-      console.log(resp);
-    });
+    .subscribe( pais => this.pais = pais);
 
     // this.activatedRoute.params.subscribe( ({id}) => {
     //   console.log(id);
